3c42170b231297d9d916a760322a202d24a22e36
Antoine Romero-Romero <ant.romero2@orange.fr>
2022-09-11 19:25:58 +0100

    feat: add a search bar to browse google

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,3 +1,39 @@
 <template>
-    <input type="text" />
+    <input type="text" v-model="searchString" @keydown.enter="search" />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data() {
+        return {
+            searchString: ''
+        }
+    },
+    methods: {
+        search() {
+            const google_address = 'https://www.google.com/search';
+            let url = google_address + '?q=' + this.searchString;
+            window.open(url);
+            this.searchString = '';
+        }
+    }
+})
+</script>
+
+<style scoped>
+    input {
+        width: 100%;
+        background-color: rgba(0, 0, 0, 0);
+        border: none;
+        border-bottom: 1px solid;
+        border-bottom-color: #606060;
+        transition: all 0.5s;
+    }
+
+    input:focus {
+        outline: none;
+        border-bottom-color: #181818;
+    }
+</style>
